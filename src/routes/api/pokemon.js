1bfb3a682b090c4f8c2ea810c19c67e9156a1d44
lilbonito <sauteboy@gmail.com>
2019-09-30 18:12:48 -0700

    initial commit

---
+++ src/routes/api/pokemon.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const superagent = require('superagent');
+// const model = require('../model/pokemon');
+// const POKEMON_API = 'https://pokeapi.co/api/v2/';
+
+router.get('/pokemon', (request, response) => {
+  superagent.get('https://pokeapi.co/api/v2/pokemon/charmander')
+    .then(result => {
+      let pokeString = JSON.stringify(result.body);
+      response.send(pokeString);
+      console.log(result.body);
+    })
+    .catch(console.error);
+});
+
+
+module.exports = router;
+
+
+
+
+
+
